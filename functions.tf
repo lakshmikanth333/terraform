1cb8ef28f4d3911dcd8f1cd2cc91804d802eab58
lakshmikanth <lakshmikanthm333@gmail.com>
2025-08-13 20:19:04 +0530

    functions

---
+++ functions.tf
@@ -0,0 +1,11 @@
+# Terraform Functions Example
+# Functions let you manipulate data dynamically and make your code reusable.
+
+variable "servers" {
+  default = ["prd", "dev"]
+}
+
+# join function: combines list items into a single string with a separator
+output "joining" {
+  value = join(".", var.servers)  # Output: "prd.dev"
+}
