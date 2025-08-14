486c734dcec27fd042863a637facd4d923d4b30d
lakshmikanth <lakshmikanthm333@gmail.com>
2025-08-14 19:38:31 +0530

    functions

--- functions/functions.tf
+++ functions/functions.tf
@@ -1,15 +1,112 @@
-# Terraform Functions Example
-# Functions let you manipulate data dynamically and make your code reusable.
+
+===== join() =====
+variable "servers" {
+  default = ["prd", "dev", "stage"]
+}
+
+output "joined_servers" {
+  value = join(".", var.servers)
+}
+
+# Output:
+# joined_servers = "prd.dev.stage"
+
+===== length() =====
 
 variable "servers" {
-  default = ["prd", "dev"]
+  default = ["prd", "dev", "stage"]
 }
 
-# join function: combines list items into a single string with a separator
-output "joining" {
-  value = join(".", var.servers)  # Output: "prd.dev"
+output "servers_count" {
+  value = length(var.servers)
 }
 
-output "length" {
-    value = length(var.servers)
+# Output:
+# servers_count = 3
+
+===== lookup() =====
+variable "zones" {
+  default = {
+    first  = "us-east-1a"
+    second = "us-east-1b"
+    third  = "us-east-1c"
+  }
 }
+
+output "zone_lookup" {
+  value = lookup(var.zones, "second", "us-east-1d")
+}
+
+# Output:
+# zone_lookup = "us-east-1b"
+
+===== format() =====
+output "formatted_message" {
+  value = format(
+    "There are %d servers running in our %s data center",
+    30,
+    "Etrade"
+  )
+}
+
+# Output:
+# formatted_message = "There are 30 servers running in our Etrade data center"
+
+===== upper() =====
+variable "cases" {
+  default = "web application"
+}
+
+output "upper_case" {
+  value = upper(var.cases)
+}
+
+# Output:
+# upper_case = "WEB APPLICATION"
+
+===== file() =====
+output "file_content" {
+  value = file("${path.module}/test.txt")
+}
+
+# Output:
+# file_content = "<contents of test.txt>"
+
+===== split() =====
+output "cloud_providers" {
+  value = split(" ", "aws gcp azure")
+}
+
+# Output:
+# cloud_providers = [
+#   "aws",
+#   "gcp",
+#   "azure",
+# ]
+
+===== merge() =====
+variable "env" {
+  default = {
+    env-1 = "prd"
+    env-3 = "stage"
+  }
+}
+
+variable "appl" {
+  default = {
+    appl-1 = "web"
+    appl-2 = "app"
+  }
+}
+
+output "merging" {
+  value = merge(var.appl, var.env)
+}
+
+# Output:
+# merging = {
+#   appl-1 = "web"
+#   appl-2 = "app"
+#   env-1  = "prd"
+#   env-3  = "stage"
+# }
